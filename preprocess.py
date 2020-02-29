6bc1511964fe123b2781e459e9e935448bd2384f
nikhildarocha <niks77883@gmail.com>
2020-02-29 23:53:24 +0530

    dropping unnecessary columns

--- preprocess.py
+++ preprocess.py
@@ -2,11 +2,26 @@
 
 import pandas as pd
 
+
+########Lowercasing##########
 print("Lowercasing")
 
 data = pd.read_csv("@Barclaycard_tweets.csv")
 
 data["text"] = data["text"].str.lower()
 
-print(data)
+print(data) #Printing
 data.to_csv('file1.csv') 
+
+########Dropping unnecessary columns##############
+
+print("Dropping the created at column")
+del data['created_at']
+
+print("Dropping the id column")
+del data['id']
+
+print(data) #Printing
+
+data.to_csv('file2.csv')
+
